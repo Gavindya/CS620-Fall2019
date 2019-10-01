0b1ac05380190666362e4ae16ef06eb4eb115d10
gavindya <nuwandijayawardena94@gmail.com>
2019-10-01 14:13:11 -0400

    complete part A

--- HW2-a.py
+++ HW2-a.py
@@ -4,39 +4,19 @@ HW2-a
 @author: Gavindya Jayawardena (UIN-01130618)
 """
 import pandas as pd
+import numpy as np
 from collections import deque
 
 """Given two list of numbers that are already sorted, 
  return a single merged list in sorted order.
 """
 def merge(sortedListA, sortedListB):
-    list_merged = deque()
-    list_A = deque(sortedListA)
-    list_B = deque(sortedListB)
+    # Using numpy to concatanate two lists
+    arr = np.concatenate((sortedListA, sortedListB))
+    # Sort the concatenated array using merge sort
+    arr.sort(kind='mergesort')
+    return arr
 
-    while list_A and list_B:
-        a = list_A[0]
-        b = list_B[0]
-        if a > b:
-            # print('a > b', a, b)
-            list_merged.append(list_B.popleft())
-            # print('list_merged', list_merged)
-        elif a == b:
-            # print('a == b', a, b)
-            list_merged.append(list_A.popleft())
-            list_merged.append(list_B.popleft())
-            # print('list_merged', list_merged)
-        else:
-            # print('a < b', a, b)
-            list_merged.append(list_A.popleft())
-            # print('list_merged', list_merged)
-    sortedList =  list_merged + list_A + list_B
-
-    return list(sortedList)
-test_list1 = [-1, 5,5, 9, 11]
-test_list2 = [-3, 4, 5, 8, 100]
-merged_lst = merge(test_list1,test_list2)
-print(merged_lst)
 
 """Given a list of numbers in random order, return the summary statistics 
 that includes the max, min, mean, population standard deviation, median,
@@ -44,15 +24,16 @@ that includes the max, min, mean, population standard deviation, median,
 """
 def summaryStatistics(listOfNums):
 
-    seriesOfNums = pd.Series(listOfNums)
+    # create a numpy array
+    arr = np.array(listOfNums)
 
-    maxVal = seriesOfNums.max()
-    minVal = seriesOfNums.min()
-    meanVal = seriesOfNums.mean()
-    stdev = seriesOfNums.std()
-    median = seriesOfNums.median()
-    perc75 = seriesOfNums.quantile(q=0.75)
-    perc25 = seriesOfNums.quantile(q=0.25)
+    maxVal = np.max(arr)
+    minVal = np.min(arr)
+    meanVal = np.mean(arr)
+    stdev = np.std(arr)
+    median = np.median(arr)
+    perc75 = np.quantile(arr, 0.75)
+    perc25 = np.quantile(arr, 0.25)
 
     return {'max': maxVal,
             'min': minVal,
@@ -62,9 +43,6 @@ def summaryStatistics(listOfNums):
             'perc75': perc75,
             'perc25': perc25}
 
-# test_list = [0, 10, 20, 20, 40, 50]
-# stats = summaryStatistics(test_list)
-# print(stats)
 
 """Given a list of real numbers in any range, scale them to be 
 between 0 and 1 (inclusive). For each number x in the list, the new number 
@@ -73,12 +51,13 @@ maximum value of the original list and min is the minimum value of the list.
 """
 
 def scaleToDigits(listOfNums):
-    seriesOfNums = pd.Series(listOfNums)
 
-    max =  seriesOfNums.max()
-    min =  seriesOfNums.min()
+    # create a numpy array
+    arr = np.array(listOfNums)
+
+    max = np.max(arr)
+    min = np.min(arr)
     diff = max - min
-    return (seriesOfNums -min)/diff
 
-# trnsformed = scaleToDigits(test_list)
-# print(trnsformed)
+    # Return Transformed array
+    return (arr -min)/diff
